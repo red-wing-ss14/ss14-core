e5c187a02ec567e24b8da9e9831afdfbe77c033c
contujeniy <nontasosal@gmail.com>
2026-07-15 19:30:24 +0300

    Update publish_multi_request.py

--- Tools/publish_multi_request.py
+++ Tools/publish_multi_request.py
@@ -74,10 +74,14 @@ def get_files_to_publish() -> Iterable[str]:
 
 
 def get_engine_version() -> str:
-    proc = subprocess.run(["git", "describe","--tags", "--abbrev=0"], stdout=subprocess.PIPE, cwd="RobustToolbox", check=True, encoding="UTF-8")
-    tag = proc.stdout.strip()
-    assert tag.startswith("v")
-    return tag[1:] # Cut off v prefix.
+    import xml.etree.ElementTree as ET
+    tree = ET.parse(os.path.join("RobustToolbox", "MSBuild", "Robust.Engine.Version.props"))
+    version = tree.getroot().find(".//Version").text.strip()
+    return version
+    # proc = subprocess.run(["git", "describe","--tags", "--abbrev=0"], stdout=subprocess.PIPE, cwd="RobustToolbox", check=True, encoding="UTF-8")
+    # tag = proc.stdout.strip()
+    # assert tag.startswith("v")
+    # return tag[1:] # Cut off v prefix.
 
 
 if __name__ == '__main__':
