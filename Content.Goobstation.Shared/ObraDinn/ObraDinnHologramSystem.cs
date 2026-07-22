06b16af59056e79aa67ee1527ad76f2951c60b85
contujeniy <nontasosal@gmail.com>
2026-07-23 00:41:49 +0300

    Update ObraDinnHologramSystem.cs

--- Content.Goobstation.Shared/ObraDinn/ObraDinnHologramSystem.cs
+++ Content.Goobstation.Shared/ObraDinn/ObraDinnHologramSystem.cs
@@ -1,12 +1,24 @@
 using Content.Shared._DV.Carrying;
 using Content.Shared._Goobstation.Wizard.Components;
 using Content.Shared._Shitmed.Medical.Surgery.Wounds.Components;
+using Content.Shared.Body.Components;
+using Content.Shared.Buckle.Components;
+using Content.Shared.Chemistry.Components.SolutionManager;
+using Content.Shared.Climbing.Components;
+using Content.Shared.Cuffs.Components;
 using Content.Shared.Damage;
+using Content.Shared.DragDrop;
 using Content.Shared.Ghost.Roles.Components;
+using Content.Shared.Hands.Components;
+using Content.Shared.Interaction;
+using Content.Shared.Interaction.Events;
+using Content.Shared.Inventory;
 using Content.Shared.Mind.Components;
 using Content.Shared.Mobs.Components;
 using Content.Shared.Movement.Components;
 using Content.Shared.Movement.Pulling.Components;
+using Content.Shared.Nutrition.AnimalHusbandry;
+using Content.Shared.Nutrition.Components;
 using Content.Shared.Speech;
 using Content.Shared.Speech.Components;
 using Content.Shared.SSDIndicator;
@@ -34,6 +46,15 @@ public sealed class ObraDinnHologramSystem : EntitySystem
         SubscribeLocalEvent<ObraDinnHologramComponent, ComponentStartup>(OnStartup);
         SubscribeLocalEvent<ObraDinnHologramComponent, ComponentShutdown>(OnShutdown);
         SubscribeLocalEvent<ObraDinnHologramComponent, InsertIntoEntityStorageAttemptEvent>(OnStorage);
+        // RW start
+        // TODO: Revert when Goobstation fixes Obra Dinn holograms retaining physical interactability (e.g. buckling to beds, drag & drop).
+        SubscribeLocalEvent<ObraDinnHologramComponent, GettingInteractedWithAttemptEvent>(OnGettingInteractedWith);
+        SubscribeLocalEvent<ObraDinnHologramComponent, InteractHandEvent>(OnInteractHand);
+        SubscribeLocalEvent<ObraDinnHologramComponent, InteractUsingEvent>(OnInteractUsing);
+        SubscribeLocalEvent<ObraDinnHologramComponent, DragDropTargetEvent>(OnDragDropTarget);
+        SubscribeLocalEvent<ObraDinnHologramComponent, CanDropTargetEvent>(OnCanDropTarget);
+        SubscribeLocalEvent<ObraDinnHologramComponent, CanDropDraggedEvent>(OnCanDropDragged);
+        // RW end
     }
 
     private void Chat(Entity<ObraDinnHologramComponent> ent, ref ListenEvent arg)
@@ -74,6 +95,22 @@ public sealed class ObraDinnHologramSystem : EntitySystem
         RemCompDeferred<CarriableComponent>(ent);
         RemCompDeferred<HasJobIconsComponent>(ent);
         RemCompDeferred<MobStateComponent>(ent);
+        // RW start
+        // TODO: Revert when Goobstation fixes Obra Dinn holograms inheriting physical mob components (butchering, breeding, buckling, bloodstream, etc.).
+        RemCompDeferred<BloodstreamComponent>(ent);
+        RemCompDeferred<BodyComponent>(ent);
+        RemCompDeferred<BuckleComponent>(ent);
+        RemCompDeferred<ButcherableComponent>(ent);
+        RemCompDeferred<ClimbableComponent>(ent);
+        RemCompDeferred<ClimbingComponent>(ent);
+        RemCompDeferred<CuffableComponent>(ent);
+        RemCompDeferred<HandsComponent>(ent);
+        RemCompDeferred<InventoryComponent>(ent);
+        RemCompDeferred<ReproductiveComponent>(ent);
+        RemCompDeferred<ReproductivePartnerComponent>(ent);
+        RemCompDeferred<SolutionContainerManagerComponent>(ent);
+        RemCompDeferred<StrapComponent>(ent);
+        // RW end
     }
 
     private void OnShutdown(Entity<ObraDinnHologramComponent> ent, ref ComponentShutdown arg)
@@ -86,4 +123,40 @@ public sealed class ObraDinnHologramSystem : EntitySystem
     {
         arg.Cancelled = true;
     }
+
+    // RW start
+    // TODO: Revert when Goobstation fixes Obra Dinn holograms interactability upstream.
+    private void OnGettingInteractedWith(Entity<ObraDinnHologramComponent> ent, ref GettingInteractedWithAttemptEvent args)
+    {
+        args.Cancelled = true;
+    }
+
+    private void OnInteractHand(Entity<ObraDinnHologramComponent> ent, ref InteractHandEvent args)
+    {
+        args.Handled = true;
+    }
+
+    private void OnInteractUsing(Entity<ObraDinnHologramComponent> ent, ref InteractUsingEvent args)
+    {
+        args.Handled = true;
+    }
+
+    private void OnDragDropTarget(Entity<ObraDinnHologramComponent> ent, ref DragDropTargetEvent args)
+    {
+        args.Handled = true;
+    }
+
+    private void OnCanDropTarget(Entity<ObraDinnHologramComponent> ent, ref CanDropTargetEvent args)
+    {
+        args.CanDrop = false;
+        args.Handled = true;
+    }
+
+    private void OnCanDropDragged(Entity<ObraDinnHologramComponent> ent, ref CanDropDraggedEvent args)
+    {
+        args.CanDrop = false;
+        args.Handled = true;
+    }
+    // RW end
 }
+
